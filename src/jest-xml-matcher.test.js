7e8145720654aef2f4305832965fee14dddc1487
Hal Blackburn <hwtb2@cam.ac.uk>
2019-09-20 16:21:17 +0100

    Re-implement pre-diff XML formatting step
    It's now more robust, pretty-data didn't normalise XML properly (e.g.
    wrapped attributes were not normalised).
    
    Now all whitespace is stripped and the document elements re-indented
    (though respecting xml:space="preserve"). The XML is serialised with
    C14N (https://www.w3.org/TR/2001/REC-xml-c14n-20010315) making it safe
    to compare as character strings.
    
    This fixes: https://github.com/kbiedrzycki/jest-xml-matcher/issues/17

--- src/jest-xml-matcher.test.js
+++ src/jest-xml-matcher.test.js
@@ -1,4 +1,4 @@
-require('./jest-xml-matcher')
+const {toEqualXML} = require('./jest-xml-matcher')
 
 const xmlOne = `<root><child name="foo" value="bar">test</child></root>`
 const xmlTwo = '<root><dad name="foo" value="baz">test</dad></root>'
@@ -21,19 +21,65 @@ describe('jest-xml-matcher', () => {
     expect(regularAttributes).toEqualXML(wrappedAttributes)
   })
 
-  it('compares different XML structures', () => {
+  it('ignores different encoding of characters', () => {
+    const plainText = `<foo>hello</foo>`
+    const escapedText = `<foo>&#104;&#101;&#108;&#108;&#111;</foo>`
+    expect(plainText).toEqualXML(escapedText)
+  })
+
+  it('ignores comments in XML', () => {
+    const plainText = `<foo><!-- hi --></foo>`
+    const escapedText = `<foo></foo>`
+    expect(plainText).toEqualXML(escapedText)
+  })
+
+  it('ignores whitespace differences', () => {
+    const xmlOne = `<root><a>  foo  </a><b><c></c></b></root>`
+    const xmlTwo = `
+      <root>
+        <a>  foo  </a>
+        <b>
+<c/>
+</b>
+      </root>
+    `
+
+    expect(xmlOne).toEqualXML(xmlTwo)
+  })
+
+  it(`doesn't ignore whitespace differences in text which is not entirely whitespace`, () => {
+    const xmlOne = `<message>Hi there!</message>`
+    const xmlTwo = `\
+<message>
+    Hi there!
+</message>`
+
     expect(xmlOne).not.toEqualXML(xmlTwo)
   })
 
-  describe('one liner check', () => {
-    it('does not find any differences', () => {
-      const xmlOne = `<root></root>`
-      const xmlTwo = `
-        <root>
-        </root>
-      `
+  it('does not ignore namespace prefix differences', () => {
+    // Perhaps it should though...
+    expect(`<a:foo xmlns:a="myns"/>`).not
+      .toEqualXML(`<b:foo xmlns:b="myns"/>`)
+  })
 
-      expect(xmlOne).toEqualXML(xmlTwo)
-    })
+  it('does not ignore default namespace differences', () => {
+    // Perhaps it should though...
+    expect(`<foo xmlns="myns"/>`).not
+      .toEqualXML(`<b:foo xmlns:b="myns"/>`)
+  })
+
+  it.each([
+    [false, `<a`, `<a/>`, 'actual value is not valid XML:'],
+    [true, `<a`, `<a/>`, 'actual value is not valid XML:'],
+    [false, `<a/>`, `<a`, 'expected value is not valid XML:'],
+    [true, `<a/>`, `<a`, 'expected value is not valid XML:'],
+    [false, `<a`, `<a`, 'actual value is not valid XML:'],
+    [true, `<a`, `<a`, 'actual value is not valid XML:']
+  ])('fails when either XML document is malformed', (isNot, xmlOne, xmlTwo, message) => {
+    const result = toEqualXML.call({isNot}, xmlOne, xmlTwo)
+    expect(result.pass).toBe(isNot);  // false when expect().ToEqualXML(), true when expect().not.toEqualXML()
+    expect(result.message()).toContain(message)
+    expect(result.message()).toContain('unexpected end of input')
   })
 })
--- src/xml-difference-checker.test.js
+++ src/xml-difference-checker.test.js
@@ -1,4 +1,4 @@
-const DiffChecker = require('./xml-difference-checker')
+const {XMLDifferenceChecker, MalformedXMLXMLDifferenceError} = require('./xml-difference-checker')
 
 let diffChecker
 
@@ -7,7 +7,7 @@ const nextValue = `<parent>test</parent>`
 
 describe('xml-difference-checker', () => {
   it('formats input values', () => {
-    diffChecker = new DiffChecker(previousValue, nextValue)
+    diffChecker = new XMLDifferenceChecker(previousValue, nextValue)
 
     const expectedNextValue = `<parent>test</parent>`
     const expectedPreviousValue = `<test>
@@ -20,19 +20,19 @@ describe('xml-difference-checker', () => {
 
   describe('#hasDifferences', () => {
     it('returns true if there are differences between xml structures', () => {
-      diffChecker = new DiffChecker(previousValue, nextValue)
+      diffChecker = new XMLDifferenceChecker(previousValue, nextValue)
       expect(diffChecker.hasDifferences).toBe(true)
     })
 
     it('returns false if xml structures are same', () => {
-      diffChecker = new DiffChecker(previousValue, previousValue)
+      diffChecker = new XMLDifferenceChecker(previousValue, previousValue)
       expect(diffChecker.hasDifferences).toBe(false)
     })
   })
 
   describe('#formattedDifferences', () => {
     it('returns string of formatted differences', () => {
-      diffChecker = new DiffChecker(previousValue, nextValue)
+      diffChecker = new XMLDifferenceChecker(previousValue, nextValue)
 
       expect(diffChecker.formattedDifferences).toContain(`<parent>test</parent>`)
     })
@@ -40,7 +40,7 @@ describe('xml-difference-checker', () => {
 
   describe('#differences', () => {
     it('returns differences between xml structures as array of diff objects', () => {
-      diffChecker = new DiffChecker(previousValue, nextValue)
+      diffChecker = new XMLDifferenceChecker(previousValue, nextValue)
 
       expect(diffChecker.differences).toEqual([
         {
@@ -56,6 +56,21 @@ describe('xml-difference-checker', () => {
           value: `<parent>test</parent>`
         }
       ])
+    })
+  })
+
+  describe('error handling', () => {
+    it.each([
+      [true, `<a`, `<a/>`],
+      [false, `<a/>`, `<a`],
+    ])('constructor throws when previous value is not valid XML', (isPrevious, previous, next) => {
+      let err
+      try { new XMLDifferenceChecker(previous, next) }
+      catch (e) { err = e; }
+
+      expect(err instanceof MalformedXMLXMLDifferenceError).toBe(true)
+      expect(err.isPrevious).toBe(isPrevious)
+      expect(err.cause.message).toContain('unexpected end of input')
     })
   })
 })
