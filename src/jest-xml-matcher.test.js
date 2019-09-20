de6bad6fea32c46f05147511025e4641285b3a87
Hal Blackburn <hwtb2@cam.ac.uk>
2019-09-20 16:20:01 +0100

    Add failing test for wrapped attribute equivalence
    Demonstrates: https://github.com/kbiedrzycki/jest-xml-matcher/issues/17

--- src/jest-xml-matcher.test.js
+++ src/jest-xml-matcher.test.js
@@ -12,6 +12,19 @@ describe('jest-xml-matcher', () => {
     expect(xmlOne).not.toEqualXML(xmlTwo)
   })
 
+  it('ignores formatting and order of attributes', () => {
+    const regularAttributes = `<foo c="3" a="1" b="2"></foo>`
+    const wrappedAttributes = `\
+<foo a="1"
+     c="3"
+     b="2"></foo>`
+    expect(regularAttributes).toEqualXML(wrappedAttributes)
+  })
+
+  it('compares different XML structures', () => {
+    expect(xmlOne).not.toEqualXML(xmlTwo)
+  })
+
   describe('one liner check', () => {
     it('does not find any differences', () => {
       const xmlOne = `<root></root>`
